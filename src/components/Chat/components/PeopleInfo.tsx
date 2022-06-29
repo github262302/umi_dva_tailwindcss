1cc93e81b934f65531f9ef2f375beda698f6f10c
github262302 <2623021604@qq,com>
2022-06-29 07:48:28 +0000

    f

--- src/components/Chat/components/PeopleInfo.tsx
+++ src/components/Chat/components/PeopleInfo.tsx
@@ -1,16 +1,25 @@
 import type { ChatModelState, PoepleSingle } from '@/models/chat'
 import { connect } from 'umi'
 import Empty from './Empty'
-
+import { useSpring, animated } from 'react-spring'
 const App = (props: { chat: ChatModelState }) => {
     const { chat, dispatch } = props
     if (chat.selectPeopleId == '') {
         return <Empty />
     }
+    const styles = useSpring({
+        loop: false,
+        from: { left: 100 },
+        to: { left: 0 },
+      })
     const userInfo =  chat.peopleList.find( (e)=>e.userId == chat.selectPeopleId)
 
     return (
-        <div className="grid place-items-center h-full w-full">
+       <div className="relative">
+            <animated.div className="grid place-items-center h-full w-full"   style={{
+         position:"absolute",
+            ...styles,
+          }}>
             <div className="w-1/2 h-5/6 flex flex-col gap-8">
                 <div className="flex gap-4">
                     <div>
@@ -48,7 +57,8 @@ const App = (props: { chat: ChatModelState }) => {
                     </div>
                 </div>
             </div>
-        </div>
+        </animated.div>
+       </div>
     )
 }
 export default connect(({ chat }: { chat: ChatModelState }) => ({
