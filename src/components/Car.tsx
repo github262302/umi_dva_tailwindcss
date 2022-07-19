d4e6ac7f2edb851f14e059e557a20d845f667472
余 <2623021604@qq.com>
2022-07-19 20:00:07 +0800

    update

--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -1,14 +1,17 @@
 import styles from "./Car.scss";
 import { useScroll } from "ahooks";
 import { useRef, useEffect, useState } from "react";
-const small =
-    "https://tesla-cdn.thron.com/delivery/public/image/tesla/8abede90-db78-460f-8e3e-122265934afc/bvlatuR/std/1928x4096/M3-Homepage-Mobile-CN";
 const big =
     "https://tesla-cdn.thron.com/delivery/public/image/tesla/c46c5993-c9c2-4938-a18d-7c414a9213ab/bvlatuR/std/4096x2560/M3-Homepage-Desktop-CN";
-
+const masaladi =
+    "https://maserati.scene7.com/is/image/maserati/maserati/international/Models/my23/mc20-cielo/16_9/MC20-cielo_design.jpg?$1920x2000$&fit=constrain";
 type PropsType = { scrolled: number };
 export default (props: PropsType): React.ReactElement => {
     const { scrolled } = props;
+    /* 返回百分比 */
+    function Pace(start: number) {
+        return (scrolled - start) / 0.1;
+    }
     function TitleStyle(): React.CSSProperties {
         let FS = 40;
         let FH = 150;
@@ -32,16 +35,54 @@ export default (props: PropsType): React.ReactElement => {
         if (scrolled > 0.1) {
             let n = 1 - 0.1 * ((scrolled - 0.1) / 0.1);
             scale = n < 0.9 ? 0.9 : n;
-        } else {
+        }
+        if (scrolled >= 0.3) {
+            let n = 0.9 + 0.1 * ((scrolled - 0.3) / 0.1);
+            scale = n > 1 ? 1 : n;
         }
         return { transform: `scale(${scale})` };
     }
-    console.log(scrolled, "scrolled");
+    function CarOne(): React.CSSProperties {
+        let left = 0;
+
+        if (scrolled >= 0.2) {
+            left = 0 - ((scrolled - 0.2) / 0.1) * 100;
+        }
+        return { left: `${left}vw` };
+    }
+    function CarTwo(): React.CSSProperties {
+        let left = 100;
+        if (scrolled >= 0.3) {
+            left = 0;
+        } else if (scrolled >= 0.2) {
+            left = 100 - ((scrolled - 0.2) / 0.1) * 100;
+        }
+        return { left: `${left}vw` };
+    }
+    function BackText(): React.CSSProperties {
+        let n = 0;
+        if (scrolled >= 0.4) {
+            let percentage = Pace(0.4);
+            n = 0.3 * percentage > 0.3 ? 0.3 : 0.3 * percentage;
+            console.log("大于4", n);
+        }
+
+        return { backgroundColor: `rgb(0, 0, 0,${n})`, ...CarTwo() };
+    }
 
     return (
         <div className={styles.main}>
             <div className={styles.content}>
-                <img className={styles.car} src={big} style={ImgStyle()} />
+                <div className={styles.cars} style={ImgStyle()}>
+                    <img className={styles.carOne} src={big} style={CarOne()} />
+                    <img
+                        className={styles.carTwo}
+                        src={masaladi}
+                        style={CarTwo()}
+                    />
+                    <div className={styles.carThree} style={BackText()}></div>
+                </div>
+
                 <div className="absolute left-1/2 animate-bounce bottom-10">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
